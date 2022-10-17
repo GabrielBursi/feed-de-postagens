46e5c6a6cb2eb792087aee498eb85ba96d4a89e6
Gabriel Bursi <gabrielcavalarosuporte@gmail.com>
2022-10-17 19:06:28 -0300

    conectando a api com fetch e fazendo o loader

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -5,9 +5,17 @@ import PostForm from '../components/PostForm';
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(()=>{
-    console.log("teste")
+    fetch('http://localhost:3001/posts').then(async (r) => {
+      const body = await r.json()
+      setPosts(body.map(post => ({
+        ...post,
+        publishedAt: new Date(post.publishedAt)
+      })))
+      setLoading(false)
+    })
   }, [])
 
   function handleSubmit({ history, userName }) {
@@ -28,6 +36,7 @@ export default function Home() {
       
       <main>
         <Feed
+          loading={loading}
           posts={posts}
           title="Seu Feed"
           subtitle="Acompanhe o que seus amigos estão pensando em tempo real"
--- front/src/pages/MostViewed.js
+++ front/src/pages/MostViewed.js
@@ -1,18 +1,27 @@
-import Feed from '../components/Feed';
+import { useState, useEffect } from "react";
+import Feed from "../components/Feed";
 
 export default function MostViewed() {
-  const posts = [
-    {
-      id: Math.random(),
-      content: 'content',
-      userName: 'Mateus',
-      publishedAt: new Date(),
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/posts/most-viewed").then(async (r) => {
+      const body = await r.json();
+      setPosts(
+        body.map((post) => ({
+          ...post,
+          publishedAt: new Date(post.publishedAt),
+        }))
+      );
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <main className="most-viewed">
       <Feed
+        loading={loading}
         posts={posts}
         title="Mais vistos"
         subtitle="Acompanhe os assuntos mais comentados no momento e fique por dentro de qualquer novidade"
