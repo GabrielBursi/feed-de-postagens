406ee79e0dae9e11f573ae546a12b9e33c3924f2
Gabriel Bursi <gabrielcavalarosuporte@gmail.com>
2022-10-17 18:41:17 -0300

    conectando a API

---
+++ front/src/pages/Home.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+import Feed from '../components/Feed';
+import PostForm from '../components/PostForm';
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    console.log("teste")
+  }, [])
+
+  function handleSubmit({ history, userName }) {
+    setPosts([
+      ...posts,
+      {
+        id: Math.random(),
+        content: history,
+        userName,
+        publishedAt: new Date(),
+      },
+    ]);
+  }
+
+  return (
+    <>
+      <PostForm onSubmit={handleSubmit} />
+      
+      <main>
+        <Feed
+          posts={posts}
+          title="Seu Feed"
+          subtitle="Acompanhe o que seus amigos estão pensando em tempo real"
+        />
+      </main>
+    </>
+  );
+}
